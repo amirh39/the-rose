604c0625266233ff15c53b75a8ff1b6878710601
unknown <a.khoshbakht79@gmail.com>
2020-09-01 01:37:11 +0300

    All finished v1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 const selectElement = (element) => document.querySelector(element);
+const revealFunction = (name, origin, delay) => {
+  window.sr = ScrollReveal();
+
+  sr.reveal(name, {
+    origin,
+    duration: 1000,
+    distance: '25rem',
+    delay,
+  });
+};
 
 const header = selectElement('header');
 const menuToggler = selectElement('.nav__toggle');
@@ -6,3 +16,8 @@ const menuToggler = selectElement('.nav__toggle');
 menuToggler.addEventListener('click', () => {
   header.classList.toggle('open');
 });
+
+revealFunction('.animate--left', 'left', 300);
+revealFunction('.animate--right', 'right', 600);
+revealFunction('.animate--top', 'top', 600);
+revealFunction('.animate--bottom', 'bottom', 600);
